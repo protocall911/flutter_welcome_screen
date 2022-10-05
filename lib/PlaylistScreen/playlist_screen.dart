43c34fa93714f7f0b1ba4a0876e54dfd4a5c057c
Sofya Naumova <pavushas@bk.ru>
2022-10-05 10:05:49 +0300

    init project

--- lib/PlaylistScreen/playlist_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:welcome_screen/PlaylistScreen/components/body.dart';
-
-class PlaylistScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white, body: Body());
-  }
-}
--- lib/WelcomeScreen/weclome_screen.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:welcome_screen/WelcomeScreen/components/body.dart';
-
-class WelcomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Body());
-  }
-}
